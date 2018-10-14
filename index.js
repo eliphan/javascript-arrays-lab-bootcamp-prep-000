aafe033b149c8638e0d1ba2f3fdf691d967125a4
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-14 06:31:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,23 +3,19 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(Ralph) {
-  kittens.push(Ralph)
-  return kittens
+  return kittens.push(Ralph)
 }
 
 function destructivelyPrependKitten(Bob) {
   return kittens.unshift(Bob)
- 
 }
 
 function destructivelyRemoveLastKitten() {
-  kittens.pop()
-  return kittens
+  return kittens.pop()
 }
 
 function destructivelyRemoveFirstKitten() {
-  kittens.shift()
-  return kittens
+  return kittens.shift()
 }
 
 function appendKitten (name) {
