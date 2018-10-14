b2c8e19096f503578f6372dfbc8c111981f1d30b
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-14 06:10:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 // Add your functions and code here
 
 function destructivelyAppendKitten(array) {
-  array.push("Ralph")
+  array.push(Ralph)
   return array
 }
 
